2d3c1d63c65679794b45d859da3a26dad68d7055
Swapnil Shukla <39062474+swapnilshukl@users.noreply.github.com>
2026-05-29 12:26:11 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto i: s){
+            if(i == ')' || i == '}' || i == ']'){
+                if(st.empty()) return false;
+                else if(i == ')' && st.top() == '(') st.pop();
+                else if(i == '}' && st.top() == '{') st.pop();
+                else if(i == ']' && st.top() == '[') st.pop();
+                else return false;
+            }else{
+                st.push(i);
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
