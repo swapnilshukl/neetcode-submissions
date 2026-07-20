4ba16eb629c50c92969af6839ebffa11e37774e8
Swapnil Shukla <39062474+swapnilshukl@users.noreply.github.com>
2026-07-20 22:12:08 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>inDegree(numCourses,0);
+
+        for(auto &p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+            inDegree[p[0]]++;
+        }
+
+        queue<int>q;
+
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i] == 0) q.push(i);
+        }
+        int count = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            count++;
+
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0) q.push(it);
+            }
+        }
+        return count == numCourses ? true : false;
+    }
+};
